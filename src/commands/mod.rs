9dc9cb5048e8f594312ec0088b52a27291f1ffbd
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-06-03 21:58:37 +0800

    fix(logging): fix logging format

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -33,19 +33,19 @@ pub async fn all_commands() -> Vec<poise::Command<Arc<Data>, Error>> {
         println!("[{}]", module.0);
 
         for safe_command in module.1.commands.into_iter() {
-            println!("{}{}{} - {}", ui::COLOR_SUCCESS, ui::CHECK, safe_command.name, safe_command.description.as_deref().unwrap_or("[no description]"));
+            println!("{}{} {} - {}", ui::COLOR_SUCCESS, ui::CHECK, safe_command.name, safe_command.description.as_deref().unwrap_or("[no description]"));
             final_commands.push(safe_command);
         }
         for conditional_command in module.1.conditional_commands.into_iter() {
             let command = conditional_command.command();
             if conditional_command.should_register() {
-                println!("{}{}{} - {}", ui::COLOR_SUCCESS, ui::CHECK, command.name, command.description.as_deref().unwrap_or("[no description]"));
+                println!("{}{} {} - {}", ui::COLOR_SUCCESS, ui::CHECK, command.name, command.description.as_deref().unwrap_or("[no description]"));
                 final_commands.push(command);
             } else {
-                println!("{}{}{} - {}", ui::COLOR_ERROR, ui::CROSS, command.name, command.description.as_deref().unwrap_or("[no description]"));
+                println!("{}{} {} - {}", ui::COLOR_ERROR, ui::CROSS, command.name, command.description.as_deref().unwrap_or("[no description]"));
             }
         }
     }
-
+    print!("{}", ui::COLOR_RESET);
     final_commands
 }
