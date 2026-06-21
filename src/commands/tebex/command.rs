49cb15a4a0836bf9e2b0b400b66c8171281a5469
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-06-21 19:33:56 +0800

    feat(tebex): implement store command functionality

--- src/commands/tebex/command.rs
+++ src/commands/tebex/command.rs
@@ -2,7 +2,9 @@ use crate::commands::ConditionalCommand;
 use crate::types::{Context, Data, Error};
 use std::sync::Arc;
 
+use poise::serenity_prelude::CreateEmbed;
 use tebex_headless_rust::handlers::misc::get_public_api_key;
+use tebex_headless_rust::handlers::webstores::get_webstore;
 
 pub struct Store;
 
@@ -11,11 +13,22 @@ pub struct Store;
 pub async fn store(ctx: Context<'_>) -> Result<(), Error> {    
     ctx.defer_ephemeral().await?;
 
+    let webstore = get_webstore().await?;
+
+    let name = webstore.name;
+    let currency = webstore.currency;
+    let domain = webstore.webstore_url;
+    //let icon = webstore.logo;
+    // let's use it later
+
     ctx.send(poise::CreateReply::default()
         .ephemeral(true)
-        .content("todo"),
-    ).await?;
-    
+        .embed(CreateEmbed::new()
+            .title(name)
+            .url(domain)
+            .description(format!(r#"Currency: **{currency}**"#))
+    )).await?;
+
     Ok(())
 }
 
