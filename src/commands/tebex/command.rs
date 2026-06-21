204a34bc1c83a93edc6a03eedd6109035fa64aa3
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-06-21 19:52:01 +0800

    feat(/store): add qr codes

--- src/commands/tebex/command.rs
+++ src/commands/tebex/command.rs
@@ -1,10 +1,13 @@
 use crate::commands::ConditionalCommand;
 use crate::types::{Context, Data, Error};
 use std::sync::Arc;
-
+use qrcode::QrCode;
+use image::Luma;
+use std::io::Cursor;
 use poise::serenity_prelude::CreateEmbed;
 use tebex_headless_rust::handlers::misc::get_public_api_key;
 use tebex_headless_rust::handlers::webstores::get_webstore;
+use poise::serenity_prelude as serenity;
 
 pub struct Store;
 
@@ -21,13 +24,25 @@ pub async fn store(ctx: Context<'_>) -> Result<(), Error> {
     //let icon = webstore.logo;
     // let's use it later
 
+    let qr_code = QrCode::new(&domain)?;
+    let img = qr_code.render::<Luma<u8>>().module_dimensions(8, 8).build();
+
+    let mut buf = Cursor::new(Vec::new());
+    img.write_to(&mut buf, image::ImageFormat::Png)?;
+
+    let attachment = serenity::CreateAttachment::bytes(buf.into_inner(), "qr.png");
+
+    let embed = CreateEmbed::new()
+        .title(name)
+        .url(domain)
+        .description(format!(r#"Currency: **{currency}**"#))
+        .thumbnail("attachment://qr.png");
+
     ctx.send(poise::CreateReply::default()
         .ephemeral(true)
-        .embed(CreateEmbed::new()
-            .title(name)
-            .url(domain)
-            .description(format!(r#"Currency: **{currency}**"#))
-    )).await?;
+        .embed(embed)
+        .attachment(attachment)
+    ).await?;
 
     Ok(())
 }
